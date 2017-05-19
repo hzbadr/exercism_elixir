66c7ff5f9b9d08e58566262b33a5d87766ded845
Hassan Zaki <hassan.zaki@badrit.com>
2017-05-20 01:43:28 +0200

    Parallel Frequency

--- elixir/parallel-letter-frequency/frequency.exs
+++ elixir/parallel-letter-frequency/frequency.exs
@@ -6,22 +6,39 @@ defmodule Frequency do
 
   The number of worker processes to use can be set with 'workers'.
   """
+  # Thanks to https://github.com/ebiven/exercism/blob/master/elixir/parallel-letter-frequency/frequency.exs
+
   @spec frequency([String.t], pos_integer) :: map
   def frequency(texts, workers) do
-    texts
-    |> Enum.flat_map(&String.graphemes(&1))
-    |> Enum.filter_map(fn(w)->
-      String.trim(w) != ""
-    end, fn(w)->
-      w
-      |> String.replace(~r/,|\.|[0-9]/, "")
-      |> String.downcase
+    results = texts
+              |> Enum.join
+              |> String.downcase
+              |> String.replace(~r/[^(.(?!\p{L}))]/u, "")
+              |> String.split("", trim: true)
+              |> do_chunk(workers)
+              |> Enum.map(&(Task.async(fn -> do_count(&1) end)))
+              |> Task.yield_many(5000)
+              |> Enum.map(fn {task, res} -> res || Task.shutdown(task, :brutal_kill) end)
+
+    for {:ok, value} <- results do
+      value
+    end
+    |> accumulate(%{})
+  end
+
+  defp do_chunk([], _), do: []
+  defp do_chunk(letters, workers) do
+    c = Enum.count(letters) / workers |> Float.ceil |> round
+    Enum.chunk(letters, c, c, [])
+  end
+  defp do_count(letters) do
+    Enum.reduce(letters, %{}, fn(letter, acc)->
+      Map.update(acc, letter, 1, &(&1 + 1))
     end)
-    |> Enum.reduce(%{}, fn(word, acc)->
-      Task.async(fn->
-        Map.update(acc, word, 1, &(&1 + 1))
-      end)
-    end)
-    |> Enum.map(&(Task.await(&1, 10000)))
+  end
+  defp accumulate([], acc), do: acc
+  defp accumulate([head | tail], acc) do
+    acc = Enum.reduce(head, acc, fn({l,c}, a) -> Map.update(a, l, c, &(&1+c)) end)
+    accumulate(tail, acc)
   end
 end
